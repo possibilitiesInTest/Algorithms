72c6926a70707070eba20143ad375fedee42437d
pit-dev <possibilitiesInTest@protonmail.com>
2021-01-28 21:44:58 -0500

    arrSquared() solution w. frequency counter pattern

--- Grider-JavaScript/arrSquared.js
+++ Grider-JavaScript/arrSquared.js
@@ -2,20 +2,45 @@
 // and checks if the elements of the second array
 // are squares of the elements in the first
 
+// function arrSquared(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     let correctIndex = arr2.indexOf(arr1[i] ** 2);
+//     if (correctIndex == -1) {
+//       return false;
+//     }
+//     console.log(arr2);
+//     arr2.splice(correctIndex, 1);
+//   }
+//   return true;
+// }
+
 function arrSquared(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    let correctIndex = arr2.indexOf(arr1[i] ** 2);
-    if (correctIndex == -1) {
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
       return false;
     }
-    console.log(arr2);
-    arr2.splice(correctIndex, 1);
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
   }
   return true;
 }
 
 arr1 = [1, 2, 3, 2];
 arr2 = [9, 1, 4, 4];
+
+arrSquared(arr1, arr2);
