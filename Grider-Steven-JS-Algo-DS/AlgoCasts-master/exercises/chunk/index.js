2f2b781f8d1aa89b4397a446b221a0b6287d93a3
pit-dev <possibilitiesInTest@protonmail.com>
2021-02-14 16:47:31 -0500

    chunked() solution2:      -create array holder for chunked      -let index equal zero      -while index is less than array length      -push slice of array(index to index + size) to chunked      -increment index by size      -return chunked

--- Grider-Steven-JS-Algo-DS/AlgoCasts-master/exercises/chunk/index.js
+++ Grider-Steven-JS-Algo-DS/AlgoCasts-master/exercises/chunk/index.js
@@ -10,18 +10,32 @@
 
 function chunk(array, size) {
   const chunked = [];
+  let index = 0;
 
-  for (let element of array) {
-    const last = chunked[chunked.length - 1];
-
-    if (!last || last.length === size) {
-      chunked.push([element]);
-    } else {
-      last.push(element);
-    }
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
 
   return chunked;
 }
+
+// function chunk(array, size) {
+//   const chunked = [];
+
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+
+//     if (!last || last.length === size) {
+//       //push current element to chunked arr
+//       chunked.push([element]);
+//     } else {
+//       // push current element to last chunk in arr
+//       last.push(element);
+//     }
+//   }
+
+//   return chunked;
+// }
 
 module.exports = chunk;
