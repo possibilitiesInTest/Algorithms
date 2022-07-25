8ddbd998d9e1f4041b58a118af214ffc0335e572
pit-dev <possibilitiesInTest@protonmail.com>
2022-07-25 14:41:41 -0400

    twoSum optimized solution w. hashMap

--- Neagoie-Andrei-Coding-Interview/twoSum/twoSumProblem.js
+++ Neagoie-Andrei-Coding-Interview/twoSum/twoSumProblem.js
@@ -15,14 +15,30 @@ set numToFind = T -nums[p1]
 let nums = [1, 3, 7, 9, 2];
 let t = 11;
 
-var twoSum = function (nums, t) {
-  for (let p1 = 0; p1 < nums.length; p1++) {
-    const numToFind = t - nums[p1];
+// brute force solution O(N^2)
+// var twoSum = function (nums, t) {
+//   for (let p1 = 0; p1 < nums.length; p1++) {
+//     const numToFind = t - nums[p1];
 
-    for (let p2 = p1 + 1; p2 < nums.length; p2++) {
-      if (numToFind === nums[p2]) {
-        return [p1, p2];
-      }
+//     for (let p2 = p1 + 1; p2 < nums.length; p2++) {
+//       if (numToFind === nums[p2]) {
+//         return [p1, p2];
+//       }
+//     }
+//   }
+//   return null;
+// };
+
+// optimized solution
+var findTwoSum = function (nums, target) {
+  const numsMap = {};
+  for (let p = 0; p < nums.length; p++) {
+    const currentMapVal = numsMap[nums[p]];
+    if (currentMapVal >= 0) {
+      return [currentMapVal, p];
+    } else {
+      const numberToFind = target - nums[p];
+      numsMap[numberToFind] = p;
     }
   }
   return null;
