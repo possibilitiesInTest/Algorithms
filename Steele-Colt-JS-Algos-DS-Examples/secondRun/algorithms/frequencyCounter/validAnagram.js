a6428a1b414c382b700e00f9356853fa56958c54
Gourav B <pit.wire@pm.me>
2022-07-28 00:52:01 -0400

    validAnagram w. frequencyCounter refactored

--- Steele-Colt-JS-Algos-DS-Examples/secondRun/algorithms/frequencyCounter/validAnagram.js
+++ Steele-Colt-JS-Algos-DS-Examples/secondRun/algorithms/frequencyCounter/validAnagram.js
@@ -6,34 +6,33 @@
 
 */
 
-
-function validAnagram(str1, str2) {
-    //check for length
-    if (str1.length !== str2.length) {
-        return false;
+function validAnagram(first, second) {
+    // check for length
+    if (first.length !== second.length) {
+      return false;
     }
-
-    // instantiate counters
-    let frequencyCounter1 = {}
-    let frequencyCounter2 = {}
-
-    // get key val, add or initialize to 1
-    for (let char of str1) {
-        frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1;
-    }
-
-    for (let char of str1) {
-        frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1;
-    }
-
-    console.log(frequencyCounter1);
-    console.log(frequencyCounter2);
-
-    // check for keys and count
-    if (frequencyCounter1 !== frequencyCounter2){
-        return false;
-    }
-    // return true
-    return true;
   
-}
+    const lookup = {};
+  
+    for (let i = 0; i < first.length; i++) {
+      let letter = first[i];
+      // if letter exists, increment, otherwise set to 1
+      lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+    console.log(lookup)
+  
+    for (let i = 0; i < second.length; i++) {
+      let letter = second[i];
+      // can't find letter or letter is zero then it's not an anagram
+      if (!lookup[letter]) {
+        return false;
+      } else {
+        lookup[letter] -= 1;
+      }
+    }
+  
+    return true;
+  }
+  
+  // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+  validAnagram('anagrams', 'nagaramm')
