e93e80bf9183381dd046d956fd5b94eefba20a5a
Gourav B <pit.wire@pm.me>
2022-07-28 00:21:10 -0400

    walkthrough frequencyCounter naive solution

--- Steele-Colt-JS-Algos-DS-Examples/secondRun/algorithms/frequencyCounter/same_naive_solution.js
+++ Steele-Colt-JS-Algos-DS-Examples/secondRun/algorithms/frequencyCounter/same_naive_solution.js
@@ -1,14 +1,26 @@
-function same(arr1, arr2){
-    if(arr1.length !== arr2.length){
+/* Write a function called same, which accepts two arrays
+    should return true, if
+    all values squared in the second arr correspond to frequency
+    of roots in the first
+
+*/
+function same(arr1, arr2) { // O(n) time
+    // check for arr.length
+    if (arr1.length !== arr2.length) {
         return false;
     }
-    for(let i = 0; i < arr1.length; i++){
-        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+    // iterate through arr1
+    for(let i = 0; i < arr1.length; i++) {
+    // let correctIndex equal to square in arr2
+    // if index does not exist, return false
+        let correctIndex = arr2.indexOf(arr1[i]**2)
         if(correctIndex === -1) {
             return false;
         }
+    // if correctIndex exists splice from arr2
+    // return true
         console.log(arr2);
-        arr2.splice(correctIndex,1)
+        arr2.splice(correctIndex, 1);
     }
     return true;
 }
