f367d4d0ec08c8a832283f425c4a4c6bbd400678
Gourav B <pit.wire@pm.me>
2022-08-08 21:23:13 -0400

    anagrams solution w. cleanString helper

--- Grider-Steven-JS-Algo-DS/secondAlgoRun/exercises/anagrams/index.js
+++ Grider-Steven-JS-Algo-DS/secondAlgoRun/exercises/anagrams/index.js
@@ -9,31 +9,39 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
-
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
+    return cleanString(stringA) === cleanString(stringB;
   }
-
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char]) {
-      return false;
-    }
-  }
-
-  return true;
+  
+  function cleanString(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
 }
-
-function buildCharMap(str) {
-  const charMap = {};
-
-  for (let char of str.replace(/[^\w]/g).toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-
-  return charMap;
-}
-
 
 module.exports = anagrams;
+
+
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA);
+//     const bCharMap = buildCharMap(stringB);
+  
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//       return false;
+//     }
+  
+//     for (let char in aCharMap) {
+//       if (aCharMap[char] !== bCharMap[char]) {
+//         return false;
+//       }
+//     }
+  
+//     return true;
+//   }
+  
+//   function buildCharMap(str) {
+//     const charMap = {};
+  
+//     for (let char of str.replace(/[^\w]/g).toLowerCase()) {
+//       charMap[char] = charMap[char] + 1 || 1;
+//     }
+  
+//     return charMap;
+//   }
