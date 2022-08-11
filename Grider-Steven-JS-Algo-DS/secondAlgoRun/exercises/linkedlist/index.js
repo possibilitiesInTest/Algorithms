e4f416d6a1732c02ce1477f566051d21c47fe9b5
Gourav B <pit.wire@pm.me>
2022-08-11 14:41:04 -0400

    linkedlist: .size()      -let counter = 0      -let node = this.head
         -while(node) {counter+++, node = node.next
         -return counter

--- Grider-Steven-JS-Algo-DS/secondAlgoRun/exercises/linkedlist/index.js
+++ Grider-Steven-JS-Algo-DS/secondAlgoRun/exercises/linkedlist/index.js
@@ -18,6 +18,17 @@ class LinkedList {
         this.head = new Node(data, this.head);
     }
 
+    size() {
+        let counter = 0;
+        let node = this.head;
+
+        while(node) {
+            counter++;
+            node = node.next;
+        }
+        return counter;
+    }
+
 }
 
 module.exports = { Node, LinkedList };
