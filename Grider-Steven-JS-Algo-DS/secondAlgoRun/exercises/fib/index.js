d333288870ce584f0ec7beb37446264af581ff98
Gourav B <pit.wire@pm.me>
2022-08-13 10:07:47 -0400

    fibonacci recursive solution

--- Grider-Steven-JS-Algo-DS/secondAlgoRun/exercises/fib/index.js
+++ Grider-Steven-JS-Algo-DS/secondAlgoRun/exercises/fib/index.js
@@ -9,16 +9,25 @@
 //   fib(4) === 3
 
 function fib(n) {
-    const result = [0, 1];
 
-    for(let i = 2; i <= n; i++) {
-        const a = result[i - 1];
-        const b = result[i - 2];
+    if(n <  2) return n;
 
-        result.push(a + b);
-    }
+    return fib(n - 1) + fib(n - 2);
 
-    return result[n];
 }
 
 module.exports = fib;
+
+
+// function fib(n) {
+//     const result = [0, 1];
+
+//     for(let i = 2; i <= n; i++) {
+//         const a = result[i - 1];
+//         const b = result[i - 2];
+
+//         result.push(a + b);
+//     }
+
+//     return result[n];
+// }
