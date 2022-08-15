82ee1e78febfcfa327f1b402ac7c71edb2623bf3
Gourav B <pit.wire@pm.me>
2022-08-14 23:29:33 -0400

    queue: weave:
    queue():
    
         -constructor() sets this.data to empty[]
         -add(record) this.data.unshift(record)
         -remove() return this.data.pop()
         -peek(data) return this.data[data.length-1]
    
    weave():
         -set q to new Queue()
         -while(sourceOne.peek() ||sourceTwo.peek()
              -q.add(sourceOne.remove()
              -q.add(sourceTwo.remove()

--- Grider-Steven-JS-Algo-DS/secondAlgoRun/exercises/weave/index.js
+++ Grider-Steven-JS-Algo-DS/secondAlgoRun/exercises/weave/index.js
@@ -25,19 +25,18 @@
 const Queue = require('./queue');
 
 function weave(sourceOne, sourceTwo) {
-    // const q = new Queue();
+    const q = new Queue();
 
-    // while(sourceOne.peek() || sourceTwo.peek()) {
-    //     if(sourceOne.peek()) {
-    //         q.add(sourceOne.remove());
-    //     }
+    while(sourceOne.peek() || sourceTwo.peek()) {
+        if(sourceOne.peek()) {
+            q.add(sourceOne.remove());
+        }
 
-    //     if(sourceTwo.peek()) {
-    //         q.add(sourceTwo.remove());
-    //     }
-    // }
-
-
+        if(sourceTwo.peek()) {
+            q.add(sourceTwo.remove());
+        }
+    }
+    return q;
 }
 
 module.exports = weave;
