66fecc5970653b3dab24268d08387a6535854fa4
Gourav B <pit.wire@pm.me>
2022-08-17 11:10:48 -0400

    selectionSort():
         -iterate through arr as i
             let indexOfMin = i
    
              -iterate through i+1 as j
                   indexOfMin = j
    
              -swap(arr, indexOfMin, i)
    
         -return arr

--- Grider-Steven-JS-Algo-DS/secondAlgoRun/exercises/sorting/index.js
+++ Grider-Steven-JS-Algo-DS/secondAlgoRun/exercises/sorting/index.js
@@ -6,7 +6,7 @@ function bubbleSort(arr) {
     for(let i = 0; i < arr.length; i++) {
         for(let j = 0; j < (arr.length - i - 1); j++) {
             if(arr[j] > arr[j+1]) {
-                const lesserOfTwo = arr[j + 1];
+                let lesserOfTwo = arr[j + 1];
                 arr[j+1] = arr[j];
                 arr[j] = lesserOfTwo;
             }
@@ -16,8 +16,29 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let indexOfMin = i;
 
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+        swap(arr, indexOfMin, i);
+    }
+    return arr;
 }
+
+function swap(arr, indexOfMin, i) {
+    if(indexOfMin !== i) {
+        // let lesserOfTwo = arr[indexOfMin];
+        // arr[indexOfMin] = arr[i];
+        // arr[i] = lesserOfTwo;  
+        
+        [arr[i], arr[indexOfMin]] =  [arr[indexOfMin], arr[i]]
+    }
+}   
+
 
 function mergeSort(arr) {
 
