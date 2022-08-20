b052c32621f9c1be16b1695b5f3b4a0ac34dee0e
Gourav B <pit.wire@pm.me>
2022-08-19 20:41:36 -0400

    twoSum(nums, target):
         -iterate through nums as p1
               set numToFind = target - nums[p1]
              -iterate through nums as p2
              of(numToFind === nums[p2])
                return [p1, p2]

---
+++ Neagoie-Andrei-Interview-directory/twoSum/index.js
@@ -0,0 +1,33 @@
+ // src: https://leetcode.com/problems/two-sum/
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// are neg?
+// are duplicate?
+// will there always be a solution available?
+
+var twoSum = function(nums, target) {
+  for(let p1 = 0; p1 < nums.length; p1++) {
+        const numberToFind = target -  nums[p1];
+    for(let p2 = p1 + 1; p2 < nums.length; p2++)
+    if(numberToFind === nums[p2]) {
+        return [p1, p2];
+    }
+  }  
+};
