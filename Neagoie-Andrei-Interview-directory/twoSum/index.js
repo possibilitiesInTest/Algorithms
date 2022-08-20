43052fc118f0d6b76c87294ae4c06e8400fb21af
Gourav B <pit.wire@pm.me>
2022-08-20 11:04:36 -0400

    twoSum(): w. hashMap:
         -create numsMap as empty object
          key = val or arr, val = index of val
    
         -iterate through each elm in arr
              -if(currentMapVal >= 0) : return [cMV, P]
              -else numToFin = target - nums[p]
                   numsMap[numToFund] = p;
    
         -return null

--- Neagoie-Andrei-Interview-directory/twoSum/index.js
+++ Neagoie-Andrei-Interview-directory/twoSum/index.js
@@ -22,12 +22,42 @@
 // are duplicate?
 // will there always be a solution available?
 
-var twoSum = function(nums, target) {
-  for(let p1 = 0; p1 < nums.length; p1++) {
-        const numberToFind = target -  nums[p1];
-    for(let p2 = p1 + 1; p2 < nums.length; p2++)
-    if(numberToFind === nums[p2]) {
-        return [p1, p2];
+/* solution using hashmap
+   {numToFind: index}
+
+
+*/
+
+const findTwoSum = function(nums, target) {
+    const numsMap = {};
+    for(let p =0; p <nums.length; p++) {
+        const currentMapVal = numsMap[nums[p]];
+        if(currentMapVal >= 0) {
+            return [currentMapVal, p];
+        } else {
+            const numberToFin = target - nums[p];
+            numsMap[numberToFind] = p;
+        }
     }
-  }  
-};
+    return null;
+}
+
+
+
+
+
+
+
+
+// var twoSum = function(nums, target) {
+//   for(let p1 = 0; p1 < nums.length; p1++) {
+//         const numberToFind = target -  nums[p1];
+//     for(let p2 = p1 + 1; p2 < nums.length; p2++) {
+//     if(numberToFind === nums[p2]) {
+//         return [p1, p2];
+//     }
+//     }
+//   } 
+//   return null; 
+// };
+
