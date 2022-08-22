36d4dea9ea255b4d53ce5ddbef818b79c92b29f5
Gourav B <pit.wire@pm.me>
2022-08-21 23:04:21 -0400

    maxAContainerArea(): w. double pointers:
         -let p1=0, p2 = h.len-1, maxArea =0;
    
         -while(p1 < p2) {
              -height = Math.mix([p1], [p2])
              -width = p2 - p1
              -area = hieght * width
              -maxArea = Math.max(maxArea, area)
    
         -if([p1] <= [p2]
              p1++
              p2--
    
         -return maxArea

--- Neagoie-Andrei-Interview-directory/maxWaterContainer/index.js
+++ Neagoie-Andrei-Interview-directory/maxWaterContainer/index.js
@@ -6,21 +6,47 @@ find the greatest area for given heights
 
 */
 
+var maxArea = function(heights) {
+    let p1 = 0, p2 =  heights.length-1, maxArea = 0;
+    
+    while(p1 < p2) {
+        const height = Math.min(heights[p1], heights[p2]);
+        const width = p2 - p1;
+        const area = height * width;
+        maxArea = Math.max(maxArea, area);
 
-
-
-
-const getMaxWaterContainer = function(heights) {
-    let maxArea = 0;
-    for(let p1 = 0; p1 < heights.length; p++) {
-        for(let p2 =p1+1; p2 < heights.length; p2++) {
-            const height = Math.min(heights[p1], heights[p2]);
-            const width = p2- p1;
-            const area = height * width;
-
-            maxArea = Math.max(maxArea, area);
+        if(heights[p1] <= heights[p2]) {
+            p1++;
+        } else {
+            p2--;
         }
-    };
+    }
 
     return maxArea;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// const getMaxWaterContainer = function(heights) {
+//     let maxArea = 0;
+//     for(let p1 = 0; p1 < heights.length; p++) {
+//         for(let p2 =p1+1; p2 < heights.length; p2++) {
+//             const height = Math.min(heights[p1], heights[p2]);
+//             const width = p2- p1;
+//             const area = height * width;
+
+//             maxArea = Math.max(maxArea, area);
+//         }
+//     };
+
+//     return maxArea;
+// }
