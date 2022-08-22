eca3ebfefac7021c95f0be16b33997decf65fb29
Gourav B <pit.wire@pm.me>
2022-08-22 14:04:10 -0400

    getTrappedRainwater():
         -let totalWaer = 0
    
         -let p < heights.len : iterate p
         -leftP/rightP = p, maxLeft/maxRight = 0;
    
         -while(leftP >= 0)
              maxLeft = Math.max(maxLeft, heights[leftP]
              leftP--
    
         -while(rightP <heights.length)
              maxRight = Math.max(maxRight, heights[rightP]
    
         -currentWater = min(maxLeft, maxRight) - heights[p]
    
         -if(currentWater >= 0) : totalWater += currentWater;
    
         -return totalWater

--- Neagoie-Andrei-Interview-directory/maxWaterContainer/index.js
+++ Neagoie-Andrei-Interview-directory/maxWaterContainer/index.js
@@ -2,8 +2,9 @@
 Given an array of heights
 find the greatest area for given heights
 
-
-
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.
 */
 
 var maxArea = function(heights) {
