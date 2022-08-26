76be23afce4b546c156e6d1006fa9929e6807769
Gourav B <pit.wire@pm.me>
2022-08-26 03:45:21 -0400

    backspaceCompare(): w. two pointers:
         -let p1/p2 = S/T.len-1;
    
         -while(p1/p2 >= 0)
    
           if (S[p1] || T[p2] = '#')
           check for S[p1] = '#'
               set backCount to 2
               while(backCount > 0)
                  decrement p1
                  decrement backCount by 1
                if new S[p1] = '#'
                increment backCount by 2
    
            if(T[p2] = '#;
              set backCount to 2
              while backCount
                 decrement p2
                 decrement backCount
               if T[p2] = '#'
               increment backCount by 2
    
            else
              S[p1] !== T[p2] : return false
              else: decrement p1/p2
    
          return true

--- Neagoie-Andrei-Interview-directory/buildString/index.js
+++ Neagoie-Andrei-Interview-directory/buildString/index.js
@@ -1,32 +1,85 @@
 
 
-
-const buildString = function(string) {
-    const builtString = [];
-    for(let p = 0; p < string.length; p++) {
-        if(string[p] !== '#') {
-            builtString.push(string[p]);
-        } else {
-            builtString.pop();
-        }
-    }
-    
-    return builtString;
-}
+const string1 = "ab#z"
+const string2 = "az#z"
 
 var backspaceCompare = function(S, T) {
-    const finalS = buildString(S);
-    const finalT = buildString(T);
+    let p1 = S.length - 1, p2 = T.length - 1;
     
-    if(finalS.length !== finalT.length) {
-        return false
-    } else {
-        for(let p = 0; p < finalS.length; p++) {
-            if(finalS[p] !== finalT[p]) {
-                return false
+    while(p1 >= 0 || p2 >= 0) {
+        if(S[p1] === "#" || T[p2] === "#") {
+            if(S[p1] === "#") {
+                let backCount = 2;
+                
+                while(backCount > 0) {
+                    p1--;
+                    backCount--;
+                    
+                    if(S[p1] === "#") {
+                        backCount += 2;
+                    }
+                }
+            }
+            
+            if(T[p2] === "#") {
+                let backCount = 2;
+                
+                while(backCount > 0) {
+                    p2--;
+                    backCount--;
+                    
+                    if(T[p2] === "#") {
+                        backCount += 2;
+                    }
+                }
+            }
+        } else {
+            if(S[p1] !== T[p2]) {
+                return false;
+            } else {
+                p1--;
+                p2--;
             }
         }
     }
     
     return true;
 };
+
+console.log(backspaceCompare(string1, string2));
+
+
+
+// solution(1):
+
+
+// const buildString = function(string) {
+//     const builtString = [];
+//     for(let p = 0; p < string.length; p++) {
+//         if(string[p] !== '#') {
+//             builtString.push(string[p]);
+//         } else {
+//             // '#'counts as backspace
+//             builtString.pop();
+//         }
+//     }
+    
+//     return builtString;
+// }
+
+// var backspaceCompare = function(S, T) {
+//     const finalS = buildString(S);
+//     const finalT = buildString(T);
+    
+//     if(finalS.length !== finalT.length) {
+//         return false
+//     } else {
+//         for(let p = 0; p < finalS.length; p++) {
+//             if(finalS[p] !== finalT[p]) {
+//                 return false
+//             }
+//         }
+//     }
+    
+//     return true;
+// };
