8fca253eb98f00ecb501d440cd49f7a0050705f7
Gourav B <pit.wire@pm.me>
2022-08-26 17:45:32 -0400

    lengthOfLongestSubstring(): w. sliding window:
         -check for s.len <= 1
          init seenChars map as {}
          init left, longest as 0
    
         -iterate through str as right
          init currentChar as s[right]
          init prevSeenChar as seenChars at currentChar
    
         -if prevSeenChar >= left bound : set left to prev + 1
    
         -update currentChar in hashmap to right
          set longest to greatest of (right - left + 1) for count of elms inbtween
    
         -return longest

--- Neagoie-Andrei-Interview-directory/longestSubString/index.js
+++ Neagoie-Andrei-Interview-directory/longestSubString/index.js
@@ -36,40 +36,77 @@ s consists of English letters, digits, symbols and spaces.
 
 */
 
-var lengthOfLongestSubstring = function(s) {
+const lengthOfLongestSubstring = function(s)  {
+// Time:  O(N)
+// Space: O(1)
 
-// Time:  O(N^2)
-// Space: O(N)
 
-  // check for edge case
-  // set longest
-  if(s.length <= 1) return s.length;
-  let longest = 0;
+    // check for s.len <= 1
+    // init seenChars charMap as {}
+    // init left, longest
+    if(s.length <=1) return s.length;
+    const seenChars = {};
+    let left = 0, longest = 0;
 
-  // iterate through str w. left pointer
-  // init seenChars hashMap
-  // init currentLength at 0
-  for(let left = 0; left < s.length; left++) {
-    let seenChars = {}, currentLength = 0;
-
-  // iterate through str w. right pointer
-  // init currentChar at s[right]
-    for(let right = left; right < s.length; right++) {
+    // iterate through s as right
+    // init currentChar as s[right]
+    // init prevSeenChar as currentChar in seenChars hashmap
+    for (let right = 0; right < s.length; right++) {
         const currentChar = s[right];
+        const prevSeenChar = seenChars[currentChar];
 
-  // if currentChar doesn't exist in seenChars
-  // increment currentLength
-  // set seenChars[currentChar] to true
-  // update longest to Math.max of currentLength
-        if(!seenChars[currentChar]) {
-            currentLength++;
-            seenChars[currentChar] = true;
-            longest = Math.max(longest, currentLength);
-
-        } else {
-            break;
+    // if prevSeenChar >= left : set left to prev + 1
+    // greater than left boundary
+        if(prevSeenChar >= left) {
+            left = prevSeenChar + 1;
         }
+
+    // update currentChar in hashmap to right val
+    // set longest to greatest of (right - left + 1)
+        seenChars[currentChar] = right;
+        longest = Math.max(longest, (right - left + 1));
     }
-  }
-  return longest;
-};
+    // return longest
+    return longest;
+}
+
+
+
+
+// var lengthOfLongestSubstring = function(s) {
+
+// // Time:  O(N^2)
+// // Space: O(N)
+
+//   // check for edge case
+//   // set longest
+//   if(s.length <= 1) return s.length;
+//   let longest = 0;
+
+//   // iterate through str w. left pointer
+//   // init seenChars hashMap
+//   // init currentLength at 0
+//   for(let left = 0; left < s.length; left++) {
+//     let seenChars = {}, currentLength = 0;
+
+//   // iterate through str w. right pointer
+//   // init currentChar at s[right]
+//     for(let right = left; right < s.length; right++) {
+//         const currentChar = s[right];
+
+//   // if currentChar doesn't exist in seenChars
+//   // increment currentLength
+//   // set seenChars[currentChar] to true
+//   // update longest to Math.max of currentLength
+//         if(!seenChars[currentChar]) {
+//             currentLength++;
+//             seenChars[currentChar] = true;
+//             longest = Math.max(longest, currentLength);
+
+//         } else {
+//             break;
+//         }
+//     }
+//   }
+//   return longest;
+// };
