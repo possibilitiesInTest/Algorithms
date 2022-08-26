c56501a39d89caa5912daf52a6cc6b3eee6dd131
Gourav B <pit.wire@pm.me>
2022-08-26 04:21:02 -0400

    lengthOfLongestSubstring(s): w. two pointers:
         -check for s.len <= 1 : return s.len
          init longest
    
         -iterate through str w. left pointer
          init seenChars hashmap
          init currentLength at 0
    
         -iterate through str w. right pointer
          init currentChar at s[right]
    
         -if currentChar doesn't exist in seenChars
          increment currentLength
          set seenChars[currentChar] to true
          update longest to Math.max of currentLength
         -else: break
    
         -return longest

--- Neagoie-Andrei-Interview-directory/buildString/index.js
+++ Neagoie-Andrei-Interview-directory/buildString/index.js
@@ -1,4 +1,33 @@
+// Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
 
+// Note that after backspacing an empty text, the text will continue empty.
+
+ 
+
+// Example 1:
+
+// Input: s = "ab#c", t = "ad#c"
+// Output: true
+// Explanation: Both s and t become "ac".
+// Example 2:
+
+// Input: s = "ab##", t = "c#d#"
+// Output: true
+// Explanation: Both s and t become "".
+// Example 3:
+
+// Input: s = "a#c", t = "b"
+// Output: false
+// Explanation: s becomes "c" while t becomes "b".
+ 
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 200
+// s and t only contain lowercase letters and '#' characters.
+ 
+
+// Follow up: Can you solve it in O(n) time and O(1) space?
 
 const string1 = "ab#z"
 const string2 = "az#z"
@@ -11,6 +40,9 @@ var backspaceCompare = function(S, T) {
             if(S[p1] === "#") {
                 let backCount = 2;
                 
+                // set backCount based on hash
+                // if backCount decrement p1
+                // decrement backCount until [p1] [p2] compare
                 while(backCount > 0) {
                     p1--;
                     backCount--;
@@ -20,7 +52,10 @@ var backspaceCompare = function(S, T) {
                     }
                 }
             }
-            
+
+              // set backCount based on hash
+              // if backCount decrement p1
+              // decrement backCount until [p1] [p2] compare
             if(T[p2] === "#") {
                 let backCount = 2;
                 
@@ -33,7 +68,9 @@ var backspaceCompare = function(S, T) {
                     }
                 }
             }
+            
         } else {
+            // [p1] [p2] compare
             if(S[p1] !== T[p2]) {
                 return false;
             } else {
